4f1db5360afe21b3cde682a1d60b56f5abc05c05
Daniel Grieve <git@cazrin.net>
2015-06-19 11:27:15 -0400

    Initial commit

---
+++ TrendMatcherTests/TrendMatcherTests.swift
@@ -0,0 +1,12 @@
+import UIKit
+import XCTest
+
+class TrendMatcherTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+}
